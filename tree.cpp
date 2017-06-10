0854365f68f1828b5e58c2779bd80956d558411c
786Macho Man <786machoman@gmail.com>
2017-06-10 22:08:51 +0530

    Working bst

--- tree.cpp
+++ tree.cpp
@@ -47,6 +47,7 @@ class Tree : public Node<T> {
         cout<<"Deleting :"<< in->data_<<endl;
         delete in;
     }
+
     virtual ~Tree()
     {
         cout<<"Destructing Tree"<<endl;
@@ -114,26 +115,27 @@ class Tree : public Node<T> {
     void remove(T in)
     {
         Node<T>* tmp = root_;
+        Node<T>* ptmp = root_;
         while(tmp)
         {
             if (in < tmp->data_) 
             {
+                ptmp = tmp;
                 tmp = tmp->lch;
             }
             else if (in > tmp->data_) 
             {
+                ptmp = tmp;
                 tmp = tmp->rch;
             }
             else if( in == tmp->data_) // Search successeded
             {
-
                 if(tmp->rch && tmp->lch) // both childs
                 {
                     //minPar is the parent Node of samallest Node
                     Node<T>* minPar = inOrderSucc(tmp->rch);
                     if(minPar)
                     {
-
                         if(!minPar->lch && !minPar->rch)
                         {
                             tmp->data_ = minPar->data_;
@@ -144,48 +146,85 @@ class Tree : public Node<T> {
                         }
                         else if(minPar->lch)
                         {
-                                tmp->data_ = minPar->lch->data_;
-                                if(minPar->lch->rch)
-                                {
-                                    Node<T>* store = minPar->lch;
-                                    minPar->lch = minPar->lch->rch ; 
-                                    cout <<"Deleting Node "<<in<<endl;
-                                    delete store;
-                                }
-                                else
-                                {
-                                    cout <<"Deleting Node "<<in<<endl;
-                                    delete minPar->lch;
-                                    minPar->lch = 0;
-                                }
-
-                                --count_;
-                                break;
+                            tmp->data_ = minPar->lch->data_;
+                            if(minPar->lch->rch)
+                            {
+                                Node<T>* store = minPar->lch;
+                                minPar->lch = minPar->lch->rch ; 
+                                cout <<"Deleting Node "<<in<<endl;
+                                delete store;
+                            }
+                            else
+                            {
+                                cout <<"Deleting Node "<<in<<endl;
+                                delete minPar->lch;
+                                minPar->lch = 0;
+                            }
+                            --count_;
+                            break;
+                        }
+                        else if(minPar->rch)
+                        {
+                            tmp->data_ = minPar->data_;
+                            tmp->rch = minPar->rch;
+                            cout <<"Deleting Node "<<in<<endl;
+                            delete minPar;
+                            --count_;
+                            break;
                         }
                     }
                 }
 
                 if(!tmp->lch && tmp->rch) // one rch
                 {
-                    tmp->data_ = tmp->rch->data_;
-                    delete tmp->rch;
-                    tmp->rch = 0;
+                    if(ptmp->lch == tmp)
+                    {
+                        ptmp->lch = tmp->rch;
+                    }
+                    else if(ptmp->rch == tmp)
+                    {
+                        ptmp->rch = tmp->rch;
+                    }
+                    delete tmp;
                     --count_;
                     break;
                 }
 
                 if(!tmp->rch && tmp->lch) // one lch
                 {
-                    tmp->data_ = tmp->lch->data_;
-                    delete tmp->lch;
-                    tmp->lch = 0;
+                    if(ptmp->lch == tmp)
+                    {
+                        ptmp->lch = tmp->lch;
+                    }
+                    else if(ptmp->rch == tmp)
+                    {
+                        ptmp->rch = tmp->lch;
+                    }
+                    delete tmp;
                     --count_;
                     break;
                 }
 
                 if(!tmp->lch && !tmp->rch) // No child case
                 {
-                    delete tmp;
+                    if(tmp == root_)
+                    {
+                        delete root_;
+                        root_ = 0;
+                        --count_;
+                        break;
+                        
+                    }
+                    if(ptmp->lch == tmp)
+                    {
+                        delete ptmp->lch;
+                        ptmp->lch = 0;
+                    }
+                    else if(ptmp->rch == tmp)
+                    {
+                        delete ptmp->rch;
+                        ptmp->rch = 0;
+                    }
                     --count_;
                     break;
                 }
@@ -255,9 +294,9 @@ int main()
     Tree<unsigned > bTree(0);
     srand((unsigned)time(0));
     int x = 0;
-    for(unsigned i = 1 ; i <= 100 ; ++i)
+    for(unsigned i = 1 ; i <= 1500000 ; ++i)
     {
-        x = (rand()%100);
+        x = (rand()%1500000);
         cout<<"Inserting : "<<x<<endl;
         bTree.insert(x);
         cout <<"Current count is: "<<bTree.getCount() <<endl; 
@@ -266,9 +305,9 @@ int main()
     bTree.preOrder();
     bTree.postOrder();
 
-    for(unsigned i = 1 ; i <= 10 ; ++i)
+    for(unsigned i = 1 ; i <= 500000 ; ++i)
     {
-        x = (rand()%100);
+        x = (rand()%1500000);
         bTree.remove(x);
     }
 
